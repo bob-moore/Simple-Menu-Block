f9325393c1ff4fd804836513d33866f777fe0174
Bob Moore <bobjutsu@gmail.com>
2025-01-16 09:22:50 -0600

    Packaged v0.1.0 for initial release "

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rcm/simple-menu-block',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a1807f6260a79565d11497be8ca93119ea9d7305',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'rcm/simple-menu-block' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a1807f6260a79565d11497be8ca93119ea9d7305',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
